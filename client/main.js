1b383afe0a936cbac941a537c83ee944bad888ad
Ante Skejic <ante.skejic@gmail.com>
2018-06-12 09:09:39 +0200

    Added validation package

--- client/main.js
+++ client/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Vuelidate from 'vuelidate';
 
 import router from './router';
+
+Vue.use(Vuelidate);
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
