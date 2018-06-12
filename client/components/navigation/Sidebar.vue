9666db814c55fa06cea301ff8a1a592e4eb5b5b3
Ante Skejic <ante.skejic@gmail.com>
2018-06-12 11:57:47 +0200

    Added user sidebar

---
+++ client/components/navigation/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <router-link to="/users/findUser" tag="li">Find User</router-link>
+      <router-link to="/users/newUser" tag="li">Create User</router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  float: left;
+  height: 90vh;
+  width: 15%;
+  background: #283648;
+  margin-top: 5px;
+  margin-left: 5px;
+  border-radius: 10px;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  padding: 10px;
+  background: #283648;
+  border-radius: 10px;
+  margin: 0;
+  color: white;
+  li {
+    padding: 10px 20px;
+    &:hover {
+      cursor: pointer;
+      background: rgba(0, 0, 0, 0.336);
+    }
+
+    &.router-link-exact-active {
+      background: rgba(0, 0, 0, 0.336);
+    }
+  }
+}
+</style>
