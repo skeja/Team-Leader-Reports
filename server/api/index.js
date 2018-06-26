baacd0ad0ef8b1740bd04ee5787df2ea33f3e793
Ante Skejic <ante.skejic@gmail.com>
2018-06-26 11:06:34 +0200

    Added report routes file

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 const auth = require('./auth');
 const user = require('./user');
+const report = require('./report');
 
 module.exports = app => {
   app.use('/api', auth);
   app.use('/api', user);
+  app.use('/api', report);
 };
