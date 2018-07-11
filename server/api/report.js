6de63107e28696859f2a977410b15acfc0e29d2e
Ante Skejic <ante.skejic@gmail.com>
2018-07-11 13:05:23 +0200

    Added authentication middleware

--- server/api/report.js
+++ server/api/report.js
@@ -2,20 +2,20 @@ const router = require('express-promise-router')();
 const ctrl = require('../controllers/report');
 const { isAuthenticated } = require('../middleware');
 
-router.get('/reports', ctrl.findBySubject);
+router.get('/reports', isAuthenticated(), ctrl.findBySubject);
 
 router.get('/reports/users', isAuthenticated(), ctrl.getLastUsersReports);
 
 router.get('/reports/team/users', isAuthenticated(), ctrl.findTeamUsersReports);
 
-router.get('/reports/:userId', ctrl.getUserReports);
+router.get('/reports/:userId', isAuthenticated(), ctrl.getUserReports);
 
-router.get('/reports/:userId/:reportId', ctrl.getReport);
+router.get('/reports/:userId/:reportId', isAuthenticated(), ctrl.getReport);
 
-router.post('/reports/:userId/newReport', ctrl.create);
+router.post('/reports/:userId/newReport', isAuthenticated(), ctrl.create);
 
-router.put('/reports/:reportId', ctrl.update);
+router.put('/reports/:reportId', isAuthenticated(), ctrl.update);
 
-router.delete('/reports/:reportId', ctrl.remove);
+router.delete('/reports/:reportId', isAuthenticated(), ctrl.remove);
 
 module.exports = router;
