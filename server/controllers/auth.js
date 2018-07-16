1fe37685421ffe14ac862bd5f96cadeb5801914c
Ante Skejic <ante.skejic@gmail.com>
2018-07-16 15:25:04 +0200

    Added http status and JSON format

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,15 +4,15 @@ const db = require('../models/');
 
 async function login({ body }, res) {
   if (!body.email || !body.password) {
-    return res.send('Input query not filled');
+    return res.status(400).jsend.error('Input query not filled');
   }
   await db.user.findOne({
     where: { email: body.email }
   }).then(async it => {
-    if (it === null) return res.send('Email & password combination not found');
+    if (it === null) return res.status(401).jsend.error('Email & password combination not found');
     const authenticated = await it.validatePassword(body.password);
-    if (!authenticated) return res.send('Email & password combination not found');
-    if (it.role === 'DEVELOPER') return res.send('Access denied');
+    if (!authenticated) return res.status(401).jsend.error('Email & password combination not found');
+    if (it.role === 'DEVELOPER') return res.status(401).jsend.error('Access denied');
     const payload = { id: it.id };
     const token = jwt.sign(payload, 'secret');
     const user = {
