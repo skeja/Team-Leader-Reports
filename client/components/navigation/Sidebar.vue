fbef19091e5df3e5673c0f90f1cb0ca8fe71f38e
Ante Skejic <ante.skejic@gmail.com>
2018-07-20 12:38:03 +0200

    Changed sidebar v-for key

--- client/components/navigation/Sidebar.vue
+++ client/components/navigation/Sidebar.vue
@@ -3,7 +3,7 @@
     <ul>
       <router-link
         v-for="{ to, label } in routes"
-        :key="to"
+        :key="label"
         :to="to"
         exact-active-class="highlighted"
         tag="li">
