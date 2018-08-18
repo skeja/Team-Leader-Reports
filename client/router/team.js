07b88a819accfe587a19daca640fd072de45769e
Ante Skejic <ante.skejic@gmail.com>
2018-08-18 14:18:47 +0200

    Removed component

--- client/router/team.js
+++ client/router/team.js
@@ -2,7 +2,6 @@ import Teams from '../components/teams/Teams.vue';
 import TeamList from '../components/teams/TeamList.vue';
 import NewTeam from '../components/teams/NewTeam.vue';
 import Team from '../components/teams/Team.vue';
-import AddUsers from '../components/teams/AddUsers.vue';
 
 export default {
   path: '/teams',
@@ -14,10 +13,6 @@ export default {
     path: 'new',
     component: NewTeam,
     name: 'newTeam'
-  }, {
-    path: 'add-users',
-    component: AddUsers,
-    name: 'addUsers'
   }, {
     path: ':id',
     component: Team,
