d3e916cc7b0b0e72fa9f5b41400414835465ed04
Ante Skejic <ante.skejic@gmail.com>
2018-08-18 14:23:51 +0200

    Added modals

---
+++ client/components/common/Confirm.vue
@@ -0,0 +1,31 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              <!-- default body -->
+            </slot>
+          </div>
+
+          <div class="modal-buttons">
+            <button @click="$emit('close')">
+              Cancel
+            </button>
+            <button @click="$emit('confirm')">
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
